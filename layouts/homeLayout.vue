a0a7304b3fa7f30280031ce93959f5b0e0bcbc42
pouria <pouria.rahimy.290@gmail.com>
2023-05-08 09:13:29 -0700

    create layout

---
+++ layouts/homeLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex w-full text-white">
+        <div class="w-[80%]">1</div>
+        <div class="w-[20%]">2</div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
