717d5b8ad5cc45df987d4aa90de0f74ab4c436d9
pouria <pouria.rahimy.290@gmail.com>
2023-05-10 09:07:00 -0700

    add pinia and creating pages

---
+++ components/layoutComponent/MenuLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex text-white justify-center items-center">
+        <!-- logo -->
+        <div class="flex"></div>
+        <div class="flex flex-col gap-3">
+            <div v-for="(item, i) in menus.menu" 
+                 class="flex justify-center items-center">
+                  <nuxt-link :to="item.link">{{ item.name }}</nuxt-link>  
+            </div>  
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    // import stores
+    import { useMenuData } from  '~/stores/MenuDate'
+
+    const menus = useMenuData()
+</script>
+
+<style lang="scss" scoped>
+
+</style>
