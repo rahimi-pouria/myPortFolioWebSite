1aa10251cafda645e0762c3b6a9338264168d437
pouria <pouria.rahimy.290@gmail.com>
2023-05-13 09:32:03 -0700

    create page

---
+++ pages/MyProject/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex">
+        this is MyProject page 
+    </div>
+</template>
+
+<script setup lang="ts">
+    definePageMeta({
+        layout: 'home-layout'
+    })
+</script>
+
+<style lang="scss" scoped>
+</style>
