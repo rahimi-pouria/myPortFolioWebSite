8fc931a588146f84c03b5795331b79f3769022b0
rahimi-pouria <pouria.rahimy.290@gmail.com>
2023-07-24 15:53:34 +0330

    end home page and data for blog and portfolio

---
+++ pages/Portfolio/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex">
+        this is portfolio page
+    </div>
+</template>
+
+<script setup lang="ts">
+    definePageMeta({
+            layout: 'home-layout'
+    })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
